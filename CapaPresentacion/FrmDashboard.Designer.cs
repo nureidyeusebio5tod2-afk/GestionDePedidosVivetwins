4bd333617908c3cf8049d8ed549d732351e9608d
TuNombre <tuemail@example.com>
2026-05-21 22:01:29 -0400

    arregle el formulario crear cliente

--- CapaPresentacion/FrmDashboard.Designer.cs
+++ CapaPresentacion/FrmDashboard.Designer.cs
@@ -288,7 +288,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1199, 645);
+            this.ClientSize = new System.Drawing.Size(1347, 645);
             this.Controls.Add(this.panel8);
             this.Controls.Add(this.flowInstalaciones);
             this.Controls.Add(this.panel4);
--- CapaPresentacion/Program.cs
+++ CapaPresentacion/Program.cs
@@ -16,7 +16,7 @@ namespace CapaPresentacion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmCliente
+            Application.Run(new FrmLogin
                 ());
         }
     }
